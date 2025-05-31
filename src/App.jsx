26f90580e2c573bad871f0fb90a97644c6498563
ron smith <ron.smith37@gmail.com>
2025-06-01 09:16:16 +1000

    More rollback of session auth pages and context

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,7 @@ import Notistack from 'components/third-party/Notistack';
 import { NotifierProvider } from 'contexts/NotifierContext';
 
 // auth-provider
-import { AuthProvider as AuthProvider } from 'contexts/SessionAuthContext';
-// import { JWTProvider as AuthProvider } from 'contexts/JWTContext';
+import { JWTProvider as AuthProvider } from 'contexts/JWTContext';
 // import { FirebaseProvider as AuthProvider } from 'contexts/FirebaseContext';
 // import { Auth0Provider as AuthProvider } from 'contexts/Auth0Context';
 // import { AWSCognitoProvider as AuthProvider } from 'contexts/AWSCognitoContext';
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,7 @@
 import { useContext } from 'react';
 
 // auth provider
-import AuthContext from 'contexts/SessionAuthContext';
-// import AuthContext from 'contexts/JWTContext';
+import AuthContext from 'contexts/JWTContext';
 // import AuthContext from 'contexts/FirebaseContext';
 // import AuthContext from 'contexts/Auth0Context';
 // import AuthContext from 'contexts/AWSCognitoContext';
